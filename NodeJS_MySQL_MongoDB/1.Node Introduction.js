2d1baac28faf92e29d7bcbc4ad477ccdc979a3f2
Pranay Singh <pranay.singh.1393@gmail.com>
2021-10-02 02:15:47 +0530

    Node JS Introduction

---
+++ NodeJS_MySQL_MongoDB/1.Node Introduction.js
@@ -0,0 +1,45 @@
+/*
+What is Node JS ?
+
+Node is not a language.
+This is a server Environment.
+Node js can connect with database that we can't do with javascript.
+Code and syntax is very similar to JS, but not exactly the same .
+Node js free and open Source .
+
+Node JS use chrome v8 engine to execute code. 
+*/
+
+/*
+Why do we use Node ?
+
+Node js mostly used for API .
+so we can connect the same database with web app, mobile app .
+Node is easy to understand who know javascript. 
+Node is super fast for APIs
+With Node and javascript we can become fullstack developer. 
+*/
+
+/*
+History and More 
+
+First release May 27,2009
+Written in C, C++ , javascript
+*/
+
+/*
+Javascript and Node are the same ?
+
+JS and Node js code syntax is same but both are not exactly same.
+we can not connect JS to DB .
+Node can connect with DB .
+Node runs on server side while Javascript runs on browser.
+*/
+
+/*
+Why should Learn ?
+If you are a JS developer then you can easy learn Node.
+We can become a full stack developer.
+Free and in demand for jobs.
+*/
+
