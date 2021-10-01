cffcb32d35a3ce20215e2aeb25a46c3125d8a2f1
Pranay Singh <pranay.singh.1393@gmail.com>
2021-10-02 02:33:13 +0530

    Node JS get started

---
+++ NodeJS_MySQL_MongoDB/2.Node Get Started.js
@@ -0,0 +1,20 @@
+//Basic before Getting Start 
+
+/*
+What are client and server side ?
+
+Client(---------->Request)Server
+  ""  (<----------Response) ""
+  JS                        Node JS
+  HTML
+  CSS 
+*/
+
+/*
+What Developers make with NOde JS ?
+
+Developers Make APIs with Node JS
+so we can connect server with client.
+Node can make API for web, Android and IOS etc .
+Node can also make website but rarely mostly API is build .
+*/
