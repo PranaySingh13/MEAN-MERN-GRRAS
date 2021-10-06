4c2a965cb6d87a8a3f8dd2a9fc1846c92fa29f52
Pranay Singh <pranay.singh.1393@gmail.com>
2021-10-07 03:27:00 +0530

    Node.js Introduction

--- NodeJS_MySQL_MongoDB/1.Node Introduction.js
+++ NodeJS_MySQL_MongoDB/1.Node Introduction.js
@@ -1,45 +1,46 @@
+//Node.js Introduction
 /*
-What is Node JS ?
-
-Node is not a language.
-This is a server Environment.
-Node js can connect with database that we can't do with javascript.
-Code and syntax is very similar to JS, but not exactly the same .
-Node js free and open Source .
-
-Node JS use chrome v8 engine to execute code. 
+What is Node.js?
+Node.js is an open source server environment
+Node.js is free
+Node.js runs on various platforms (Windows, Linux, Unix, Mac OS X, etc.)
+Node.js uses JavaScript on the server
 */
 
 /*
-Why do we use Node ?
+Why Node.js?
+Node.js uses asynchronous programming!
 
-Node js mostly used for API .
-so we can connect the same database with web app, mobile app .
-Node is easy to understand who know javascript. 
-Node is super fast for APIs
-With Node and javascript we can become fullstack developer. 
+A common task for a web server can be to open a file on the server and return the content to the client.
+
+Here is how PHP or ASP handles a file request:
+
+Sends the task to the computer's file system.
+Waits while the file system opens and reads the file.
+Returns the content to the client.
+Ready to handle the next request.
+Here is how Node.js handles a file request:
+
+Sends the task to the computer's file system.
+Ready to handle the next request.
+When the file system has opened and read the file, the server returns the content to the client.
+Node.js eliminates the waiting, and simply continues with the next request.
+
+Node.js runs single-threaded, non-blocking, asynchronously programming, which is very memory efficient.
 */
 
 /*
-History and More 
-
-First release May 27,2009
-Written in C, C++ , javascript
+What Can Node.js Do?
+Node.js can generate dynamic page content
+Node.js can create, open, read, write, delete, and close files on the server
+Node.js can collect form data
+Node.js can add, delete, modify data in your database
 */
 
 /*
-Javascript and Node are the same ?
-
-JS and Node js code syntax is same but both are not exactly same.
-we can not connect JS to DB .
-Node can connect with DB .
-Node runs on server side while Javascript runs on browser.
+What is a Node.js File?
+Node.js files contain tasks that will be executed on certain events
+A typical event is someone trying to access a port on the server
+Node.js files must be initiated on the server before having any effect
+Node.js files have extension ".js"
 */
-
-/*
-Why should Learn ?
-If you are a JS developer then you can easy learn Node.
-We can become a full stack developer.
-Free and in demand for jobs.
-*/
-
