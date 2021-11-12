fb18eadc68898a7e73cf39c91b5d6cfc529b5921
Pranay Singh <pranay.singh.1393@gmail.com>
2021-11-13 04:19:37 +0530

    Node.js MongoDB Intro

---
+++ NodeJS_MySQL_MongoDB/23.Node MongoDB Intro.js
@@ -0,0 +1,30 @@
+//Node.js MongoDB
+
+/**
+ * Node.js can be used in database applications.
+
+One of the most popular NoSQL database is MongoDB.
+
+MongoDB
+To be able to experiment with the code examples, you will need access to a MongoDB database.
+
+You can download a free MongoDB database at https://www.mongodb.com.
+
+Or get started right away with a MongoDB cloud service at https://www.mongodb.com/cloud/atlas.
+ */
+
+/**
+ * Install MongoDB Driver
+Let us try to access a MongoDB database with Node.js.
+
+To download and install the official MongoDB driver, open the Command Terminal and execute the following:
+
+Download and install mongodb package:
+
+C:\Users\Your Name>npm install mongodb
+Now you have downloaded and installed a mongodb database driver.
+
+Node.js can use this module to manipulate MongoDB databases:
+
+var mongo = require('mongodb');
+ */
