ca26b9420b8c61a4b42e481d7056ddff13a68da7
Pranay Singh <pranay.singh.1393@gmail.com>
2021-11-17 21:39:26 +0530

    nodemon module for time saving

---
+++ NodeJS_MySQL_MongoDB/36.Node nodemon module.js
@@ -0,0 +1,14 @@
+//Nodemon package is time saving module.
+
+/**
+ * Assume there is a statement at line printing "Hi". What we do for run the file , we go for node filename.js in terminal.
+ 
+Suppose you again edit the file so you have to run again the file.
+
+Therefore, nodemon package helps you in time saving while running file.
+
+As you run the file at start with nodemon filename.js and you edit the file and save it. It will automatically run the updated file giving output. 
+ */
+console.log("Hi");
+console.log("Hello");
+console.log(4 + 5);
